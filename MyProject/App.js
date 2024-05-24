d025dc44ebcc384bdedb367aa98b7f69dddbf085
Antwiwaa <nanaafuaantwiwaa624@gmail.com>
2024-05-24 17:34:46 +0000

    changed bgcolor

--- MyProject/App.js
+++ MyProject/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.container} >
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
